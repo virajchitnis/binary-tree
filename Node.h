dad16f58fb32c0c861528f58a95d34f55f53b454
Viraj Chitnis <chitnisviraj@gmail.com>
2018-12-02 15:48:15 +0000

    Added Node class

---
+++ Node.h
@@ -0,0 +1,25 @@
+#include <string>
+
+using namespace std;
+
+#ifndef NODE
+#define NODE
+class Node {
+private:
+  string _value;
+  Node *_left_child;
+  Node *_right_child;
+public:
+  Node(string value) : _value(value) {}
+
+  string value() { return _value; }
+  Node* left_child() { return _left_child; }
+  Node* right_child() { return _right_child; }
+
+  void set_left_child(Node *lchild) { _left_child = lchild; }
+  void set_right_child(Node *rchild) { _right_child = rchild; }
+
+  bool has_left_child() { if(_left_child != 0) return true; return false; }
+  bool has_right_child() { if(_right_child != 0) return true; return false; }
+};
+#endif
